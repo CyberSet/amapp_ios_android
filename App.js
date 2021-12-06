f374617337475819bf9728ae4e15efa8c9569fcc
anton pastukhov <antonpastukhov@MacBook-Air-anton.local>
2021-12-06 18:17:01 +0300

    notifiacations

--- App.js
+++ App.js
@@ -11,12 +11,17 @@ import { store } from './store/Store';
 import { Provider } from 'react-redux';
 import { UserNavigator } from './navigation/UserNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen'
 
-const App = () => (
-  <Provider store={store}>
-    <NavigationContainer>
-      <UserNavigator />
-    </NavigationContainer>
-  </Provider>
-);
+const App = () => {
+  SplashScreen.hide();
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <UserNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
+}
 export default App;
